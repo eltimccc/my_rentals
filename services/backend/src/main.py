4645382035dee7a2b28d753ff6357e79368ad04a
Denis M <eltimccc@gmail.com>
2023-08-19 00:19:47 +0300

    edit docs url in main.py

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -6,7 +6,8 @@ from src.api.routers import main_router
 
 
 app = FastAPI(title=settings.app_title,
-              description=settings.description)
+              description=settings.description,
+              docs_url='/swagger')
 
 
 app.include_router(main_router)
